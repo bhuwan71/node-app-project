1a406533f3196848713b64be9a9379f0e3c711fa
Rajeev9813 <bhuwanchettri71@gmail.com>
2025-03-24 21:42:08 +0545

    changes

--- app/routes/kundali.routes.js
+++ app/routes/kundali.routes.js
@@ -1,33 +1,33 @@
-// module.exports = (app) => {
-//     const {
-//       createKundali,
-//       getAllKundalis,
-//       getKundaliById,
-//       updateKundali,
-//       deleteKundali,
-//       matchKundalis
-//     } = require("../controllers/kundali.controller.js");
+module.exports = (app) => {
+    const {
+      createKundali,
+      getAllKundalis,
+      getKundaliById,
+      updateKundali,
+      deleteKundali,
+      matchKundalis
+    } = require("../controllers/kundali.controller.js");
   
-//     var router = require("express").Router();
+    var router = require("express").Router();
   
-//     // Create: Add a new Kundali
-//     router.post("/", createKundali);
+    // Create: Add a new Kundali
+    router.post("/", createKundali);
   
-//     // Read (All): Get all Kundali records
-//     router.get("/", getAllKundalis);
+    // Read (All): Get all Kundali records
+    router.get("/", getAllKundalis);
   
-//     // Read (Single): Get a specific Kundali record by ID
-//     router.get("/:id", getKundaliById);
+    // Read (Single): Get a specific Kundali record by ID
+    router.get("/:id", getKundaliById);
   
-//     // Update: Update a specific Kundali record by ID
-//     router.put("/:id", updateKundali);
+    // Update: Update a specific Kundali record by ID
+    router.put("/:id", updateKundali);
   
-//     // Delete: Delete a specific Kundali record by ID
-//     router.delete("/:id", deleteKundali);
+    // Delete: Delete a specific Kundali record by ID
+    router.delete("/:id", deleteKundali);
     
-//     router.post("/match", matchKundalis);
+    router.post("/match", matchKundalis);
 
   
-//     app.use("/api/kundali", router);
-//   };
+    app.use("/api/kundali", router);
+  };
   
